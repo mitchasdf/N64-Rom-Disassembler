5bbb281ba16a0b9d71706abc13844f7368ed7bb4
Mitchell <mitch.asdf@outlook.com>
2018-01-25 19:24:55 +1000

    add todo file

---
+++ todo.py
@@ -0,0 +1,24 @@
+
+'just pretend this string is not here'
+
+
+'''  ADDITIONS
+
+Create a button for the jumps window which labels all jumps to a function using the function title comment.
+
+'''
+
+
+'''  BUGS
+
+None I know of right now.
+
+'''
+
+
+'''  CHANGES
+
+Tweak jump mapping to be a little smarter. I aim to have it calculate target addresses for JR/JALR instructions
+  if the JR target is statically calculated.
+
+'''
