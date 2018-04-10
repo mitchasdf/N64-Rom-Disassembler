96c10a9f0b7e762a2337bd3036ed947c689f20f5
Mitchell <mitch.asdf@outlook.com>
2018-04-11 06:48:58 +1000

    Updated patch server script

--- pj64d patch server script.js
+++ pj64d patch server script.js
@@ -1,17 +1,9 @@
 
-socket = new Socket();
-server = new Server({'port': 8329});
-settings = new Object();
-settings['host'] = '127.0.0.1';
-settings['port'] = 8319;
-ints = new Object();
-logging = false;
+// Put me into the PJ64d scripts directory please
 
-// try {
-socket.connect(settings, function() {
-    console.log('Successfully connected.');
-    console.log('Use log() to toggle task logging. Disabled by default.');
-});
+server = new Server({'port': 8329});
+ints = {};
+logging = false;
 
 server.on('connection', function(sock) {
     sock.on('data', function(data) {
@@ -39,41 +31,10 @@ server.on('connection', function(sock) {
     });
 });
 
-
-// } catch(e) {
-//     console.log('asdf');
-// }
-//
 var log = function() {
     logging = !logging;
     console.log('logging = ' + logging);
 };
 
-var send = function(addr, val) {
-    console.log('sending ' + addr + ' ' + val)
-    socket.write(addr + '\n' + val, function(response) {
-        if (logging) {
-            console.log(response);
-        }
-    });
-    // Will recieve data to print which instruction was written
-};
-
-
-// I think this method would stop a lot of unnecessary data transfer between programs
-//   at the cost of a few MB of RAM.
-//var v = 0;
-//events.onexec(ADDR_ANY, function(addr) {
-//    key = addr.toString();
-//    this_val = mem.u32[addr];
-//    if (key in ints) {
-//       if (this_val != ints[key]) {
-//           send(addr, this_val);
-//       }
-//    }
-//    ints[key] = this_val;
-//    if (v++ < 100) {
-//        console.log(key);
-//        console.log(ints[key]);
-//    }
-//});
+console.log('You can now simply apply patches with Ctrl+P in the disassembler.');
+console.log('Use log() to toggle task logging (default: disabled)');
